e78eab10ae317d6ffd88c519e753a78afa34f2e4
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-08-28 15:47:44 +0530

    added file upload component

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import PrimeVue from "primevue/config";
+
+import FileUpload from "primevue/fileupload";
+
+createApp(App)
+    .use(router)
+    .use(PrimeVue)
+    .component("FileUpload", FileUpload)
+    .mount('#app')
